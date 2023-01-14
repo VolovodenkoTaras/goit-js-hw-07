0dfd29f6895869128deb76d7bc1e0dfd0149c067
Volovodenko Taras <taras.volovodenko@gmail.com>
2023-01-14 21:28:07 +0200

    sum

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,34 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+// console.log(basicLightbox);
+
+// const instance = basicLightbox.create(`
+// 	<h1>Dynamic Content</h1>
+// 	<p>You can set the content of the lightbox with JS.</p>
+// `)
+// // instance.show()
+// instance.show(() => console.log('lightbox now visible'))
+
+// setTimeout(instance.close(() => console.log('lightbox not visible anymore')), 5000)
+
+const instance = basicLightbox.create(`
+    <div class="modal">
+        <p>
+            Your first lightbox with just a few lines of code.
+            Yes, it's really that simple.
+        </p>
+    </div>
+`, {
+    onShow: (instance) => {
+        console.log("hello world!")
+    },
+})
+
+instance.show()
+
+// instance.close()
+
+
+new SimpleLightbox('.test a', { /* options */ });
